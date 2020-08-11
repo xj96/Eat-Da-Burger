59b931e6df902b5d5212b4a4284eed7e9a4932d9
Xavier Jackson <xavierejackson20@gmail.com>
2020-08-11 02:01:23 -0400

    added in connection to heroku and formatted personal content

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
+require("dotenv").config()
+
 // Set up MySQL connection.
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "xej62920",
-  database: "burgers_db"
-});
+const connection
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.PW,
+    database: "burgers_db"
+  })
+}
+
 
 // Make connection.
 connection.connect(function (err) {
